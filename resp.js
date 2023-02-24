0727ab8d8f2710d173fd980e4216f633ab7b1959
RaviShankar1s <124431345+RaviShankar1s@users.noreply.github.com>
2023-02-25 02:02:52 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,21 @@
+
+
+        
+/* Login ke Liye Script Hai */
+var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    var z = document.getElementById("btns");
+
+    function register(){
+        x.style.left = "-400px";
+        y.style.left = "50px";
+        z.style.left = "110px";
+    }
+    function login(){
+        x.style.left = "50px";
+        y.style.left = "450px";
+        z.style.left = "0px";
+    }
+
+    
+
